8f99330c12aa95545c04403ef1feebe37f4f7222
jane <82953024+git-jane-hub@users.noreply.github.com>
2021-08-14 23:41:54 +0900

    question6

---
+++ src/step1/P10.java
@@ -0,0 +1,24 @@
+package step1;
+
+import java.util.Scanner;
+
+
+public class P10 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int nkg = scan.nextInt();
+		int num = 0;
+		int bongji;
+		for(int a = 1; a <= 3; a++) {
+			
+		
+			if(nkg % 5 == nkg % 3) {
+				
+			}else {
+				bongji = -1;
+				System.out.println(bongji);
+			
+			}
+		}
+	}
+}
