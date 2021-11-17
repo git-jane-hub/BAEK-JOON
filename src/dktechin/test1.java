0a38ce3d277262516783ee7d4bf927969e8594f7
jane <choiwodls@naver.com>
2021-11-17 23:52:04 +0900

    dktechin

--- src/dktechin/test1.java
+++ src/dktechin/test1.java
@@ -2,16 +2,19 @@ package dktechin;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class test1 {
 	static String getRandomString(int i) {
-		 byte[] bytearray = new byte[8];	// 문자열 최대 바이트 
+		 byte[] bytearray = new byte[8];
 		 String str;
 		 StringBuffer buffer;
 		 String alphabet;
@@ -30,7 +33,7 @@ public class test1 {
 				 i--;
 			 }
 		 }
-		 return buffer.toString();	// 생성만
+		 return buffer.toString();
 	}
 	
 	public static void main(String[] args) {
@@ -40,19 +43,19 @@ public class test1 {
 		
 		// 1번 문제
 		HashMap map = new HashMap();
-		int i = 4;	// 문자열 길이
+		final int STRINGLENGTH = 4;
 		
-		while(map.size() <= 70) {	// 70000으로 변경 
-			String ranStr = getRandomString(i);
+		while(map.size() <= 7000000) {
+			String ranStr = getRandomString(STRINGLENGTH);
 			int ranNum = (int)(Math.random() * 9999) + 1;
-			if(!map.containsKey(ranStr) && ranStr != "") {
+			if(!map.containsKey(ranStr) && !ranStr.equals("")) {
 				map.put(ranStr, ranNum);
 			}
 		}
 		
 		// 1번 문제 끝
 		Long endTime = System.currentTimeMillis();
-		System.out.println("1번 실행시간: " + (endTime - startTime)/1000.0 + "초");
+		System.out.println("1번 실행시간: " + (endTime - startTime)/1000.0 + "초\n");
 		
 		// 2번 문제 시작
 		startTime = System.currentTimeMillis();
@@ -60,56 +63,68 @@ public class test1 {
 		// 2번 문제
 		String inputStr = "ab";	// 찾으려는 문자열 입력 
 		if(map.containsKey(inputStr)) {
-			System.out.println("추출된 값: " + map.get(inputStr));
+			System.out.println("- 입력된 값");
+			System.out.println(inputStr);
+			System.out.println("- 추출된 값");
+			System.out.println(map.get(inputStr));
 		}
+		
 		// 2번 문제 끝
 		endTime = System.currentTimeMillis();
-		System.out.println("2번 실행시간: " + (endTime - startTime)/1000.0 + "초");
+		System.out.println("2번 실행시간: " + (endTime - startTime)/1000.0 + "초\n");
 		
-		/*
-		HashMap map = new HashMap();
-		map.put("qwe", 243);
-		map.put("asd", 2354);
-		map.put("ss", 2354);
-		*/
 		// 3번 문제 시작
 		startTime = System.currentTimeMillis();
 				
 		// 3번 문제
 		int inputInt = 2354;	// 찾으려는 숫자 입력
 		
-		Set set = map.entrySet();	// 모든 값을 가져옴
+		Set set = map.entrySet();
 		Iterator it = set.iterator();
 		
+		System.out.println("- 추출된 결과");
 		while(it.hasNext()) {
 			Map.Entry e = (Map.Entry)it.next();
-			if((Integer)e.getValue() == inputInt) {
-				System.out.println("추출된 결과: " + e.getKey());	// 첫 값일 경우 못가져옴
+			if((int)e.getValue() == inputInt) {
+				System.out.print(e.getKey() + ", ");
 			}
-			
 		}
-		
-		if(map.containsValue(inputInt)) {
-			System.out.println("다음 숫자에 매칭되어있는 문자열 추출: " + map.get(inputInt));
-		}
+		System.out.println();
 		
 		// 3번 문제 끝
 		endTime = System.currentTimeMillis();
-		System.out.println("3번 실행시간: " + (endTime - startTime)/1000.0 + "초");
+		System.out.println("3번 실행시간: " + (endTime - startTime)/1000.0 + "초\n");
 		
 		// 4번 문제 시작
 		startTime = System.currentTimeMillis();
 		
 		// 4번 문제
 		inputStr = "qwe"; // 찾으려는 문자열 입력
-		for(int j = 0; j < inputStr.length(); j++) { 	// 한문자씩 분리하고(이거됨) 하나씩 더해야하는데 그게 안됨,,,,
-			String result = inputStr.charAt(j) + "";
-			System.out.println(result);
+		System.out.println("- 입력된 값");
+		System.out.println(inputStr);
+		String inputStrEach = "";
+		int [] inputStrArr = new int[inputStr.length()];
+		System.out.println("- 추출된 결과");
+		for(int j = 0; j < inputStr.length(); j++) { 
+			inputStrEach += inputStr.charAt(j) + "";
+			inputStrArr[j] = (int)map.get(inputStrEach);
+			System.out.println(inputStrEach + " -> " + inputStrArr[j]);
 		}
+		Arrays.sort(inputStrArr);
+		IntStream.of(inputStrArr).boxed().collect(Collectors.groupingBy(i -> i))
+			.entrySet()
+			.stream()
+			.filter(e -> e.getValue().size() >= 1)
+			.forEach(e -> {
+				System.out.println(e.getKey() + " : " + e.getValue()
+				.stream()
+				.collect(Collectors.counting()) + "개");
+		});
 		
 		// 4번 문제 끝
 		endTime = System.currentTimeMillis();
-		System.out.println("4번 실행시간: " + (endTime - startTime)/1000.0 + "초");
+		System.out.println("4번 실행시간: " + (endTime - startTime)/1000.0 + "초\n");
 		
 	}
+	
 }
--- src/dktechin/testtest.java
+++ src/dktechin/testtest.java
@@ -1,11 +1,39 @@
 package dktechin;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class testtest {
 	public static void main(String[] args) {
-		String inputStr = "qwe"; // 찾으려는 문자열 입력
-		for(int j = 0; j < inputStr.length(); j++) { 
-			String result = inputStr.charAt(j) + "";
-			System.out.println(result);
-		}
+		/*
+		int[] inputStrArr = {1, 3, 3, 5, 7, 8, 8, 8};
+		IntStream.of(inputStrArr)
+        .boxed()
+        .collect(Collectors.groupingBy(i -> i))
+        .entrySet()
+        .stream()
+        .filter(e -> e.getValue().size() > 1)
+        .forEach(e -> {
+            System.out.println(e.getKey() + " -> " + e.getValue()
+                                         .stream()
+                                         .collect(Collectors.counting()));
+        });
+        */
+		int[] inputStrArr = {1, 3, 3, 5, 7, 8, 8, 8};
+		IntStream.of(inputStrArr)
+		.boxed()
+		.collect(Collectors.groupingBy(i -> i))
+		.entrySet()
+		.stream()
+		.filter(e -> e.getValue().size() >= 1)
+		.forEach(e -> {
+			System.out.println(e.getKey() + " -> " + e.getValue()
+			.stream()
+			.collect(Collectors.counting()) + "\n");
+		});
+		
+		System.out.println("-----");
+		System.out.println();
+		System.out.println("-----");
 	}
 }
